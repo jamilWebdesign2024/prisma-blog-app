c34018784de75b92931f7bd0dd7644e4717b4cec
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2026-01-14 10:38:26 +0600

    all ok

--- src/moules/post/post.controller.ts
+++ src/moules/post/post.controller.ts
@@ -3,7 +3,15 @@ import { PostService } from "./post.service";
 
 const createPost = async(req:Request, res:Response)=>{
     try{
-        const result = await PostService.createPost(req.body)
+      const user = req.user
+
+      if(!user){
+        return res.status(400).json({
+        error: "unauthorized",
+         })
+      }
+      
+        const result = await PostService.createPost(req.body, user.id as string)
         res.status(201).json(result)
     }catch(err){
       res.status(400).json({
--- src/moules/post/post.router.ts
+++ src/moules/post/post.router.ts
@@ -1,8 +1,14 @@
-import express from 'express';
+import express, {Router} from 'express';
 import { PostController } from './post.controller';
+
+import auth, { UserRole } from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.post("/", PostController.createPost)
+
+
+router.post("/", 
+    auth(UserRole.USER),    
+    PostController.createPost)
 
 export const postRouter = router;
