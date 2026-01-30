d4efbab251db9ab3b4b5651ef749d986db6df160
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2026-01-30 17:55:15 +0600

    all commit

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -26,14 +26,14 @@ async function seedAdmin() {
         console.log("👤 user not found");
         
         // Use hardcoded URL instead of env variable
-        const authUrl = "http://localhost:3000/api/auth/sign-up/email";
+        const authUrl = "http://localhost:5000/api/auth/sign-up/email";
         console.log(`📍 Calling: ${authUrl}`);
 
         const signUpAdmin = await fetch(authUrl, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
-                "Origin": "http://localhost:4000"
+                "Origin": "http://localhost:3000"
             },
             body: JSON.stringify(adminData)
         });
